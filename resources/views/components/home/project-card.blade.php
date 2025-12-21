a78088b078e185bc2c5cabd53584af91c9e7eac9
Brandon <brandonnilly@gmail.com>
2025-12-21 15:35:44 -0700

    Fix Project Card Image Overflow

--- resources/views/components/home/project-card.blade.php
+++ resources/views/components/home/project-card.blade.php
@@ -5,20 +5,22 @@
     'tags' => [],
     'href' => '#'
 ])
-<a href="{{ $href }}" class="relative w-full h-full group">
-    <div class="z-[1] relative block flex flex-col justify-between w-full h-full bg-gray-800 rounded-lg overflow-hidden duration-200 group-hover:-translate-y-1 group-hover:-translate-x-1">
-        <div class="relative h-48">
-            @if(App::isProduction())
-                <img src="{{ $image }}" alt="Featured image for {{ $title }}" class="w-full object-cover object-center">
-            @else
-                <img src="{{ $image }}" alt="Featured image for {{ $title }}" class="w-full object-cover object-center">
-            @endif
-        </div>
-        <div class="px-3 pb-4">
-            <h4 class="text-xl font-bold text-gray-100 mb-3 line-clamp-1">{{ $title }}</h4>
-            <p class="text-gray-300 line-clamp-3">
-                {{ $description }}
-            </p>
+<a href="{{ $href }}" class="relative block group">
+    <div class="z-[1] relative flex flex-col justify-between bg-gray-800 h-full rounded-lg overflow-hidden duration-200 group-hover:-translate-y-1 group-hover:-translate-x-1">
+        <div>
+            <div class="relative h-32">
+                @if(App::isProduction())
+                    <img src="{{ $image }}" alt="Featured image for {{ $title }}" class="w-full h-full object-cover object-center">
+                @else
+                    <img src="{{ $image }}" alt="Featured image for {{ $title }}" class="w-full h-full object-cover object-center">
+                @endif
+            </div>
+            <div class="px-3 pt-3 pb-4">
+                <h4 class="text-xl font-bold text-gray-100 mb-3 line-clamp-1">{{ $title }}</h4>
+                <p class="text-gray-300 line-clamp-3">
+                    {{ $description }}
+                </p>
+            </div>
         </div>
         <div class="px-3 pb-4 flex flex-wrap items-center gap-3">
             @foreach($tags as $tag)
