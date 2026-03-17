c4e2dce130f4a4ab33e3f8b3066f8d37de389af6
Brandon <brandonnilly@gmail.com>
2026-03-17 17:07:06 -0600

    Default Panels For Filament

---
+++ app/Filament/Resources/Projects/Schemas/ProjectForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Projects\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ProjectForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('slug')
+                    ->required(),
+                TextInput::make('title')
+                    ->required(),
+                FileUpload::make('featured_image')
+                    ->image()
+                    ->required(),
+                TextInput::make('description')
+                    ->required(),
+                Textarea::make('body')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+}
