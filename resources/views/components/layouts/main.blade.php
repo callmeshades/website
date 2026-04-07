226534ab72ca5ace19824d6ae4962ef24717d802
Brandon <brandonnilly@gmail.com>
2026-04-07 16:41:56 -0600

    Add Plausible

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -8,6 +8,11 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
+    <script async src="https://plausible.io/js/pa-_xNUsRWrVqUU1zxDHtGdK.js"></script>
+    <script>
+        window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+        plausible.init()
+    </script>
 </head>
 <body class="bg-gray-900">
 <x-navbar/>
